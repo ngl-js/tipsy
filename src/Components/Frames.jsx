efa7aed78d9e5f6a6ab7b1e42288c71155636aee
ngl-js <joshua.dq@outlook.com>
2025-05-30 07:39:42 -0500

    assets by id

--- src/Components/Frames.jsx
+++ src/Components/Frames.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { baseURL } from "../services/http";
+import { setAssetsUrl } from "../services/http";
 // otros
 import noImage from "../assets/img/no-image.jpg";
 // icons
@@ -11,33 +11,30 @@ import RatingStars from "./RatingStars";
 import { MediaContext } from "../context/MediaContext";
 
 const Frames = () => {
+  const { assets, selectedImg, setSelectedFrame, selectedAudio } =
+    useContext(MediaContext);
 
-  const { 
-    assets,
-    selectedImg, 
-    setSelectedFrame,
-    selectedAudio
-  }= useContext(MediaContext)
+  const img_url = setAssetsUrl();
 
-  const [url, setUrl]= useState(noImage)
-  const [focus, setFocus]= useState();
+  const [url, setUrl] = useState(noImage);
+  const [focus, setFocus] = useState();
 
-  const handleUrl= (selected) => {
-    const _url= `${baseURL}/img/frames/${selected}`
-    setUrl(_url)
-    setFocus(selected)
-  }
+  const handleUrl = (selected) => {
+    const _url = `${img_url}/${selected}`;
+    setUrl(_url);
+    setFocus(selected);
+  };
 
-  const selectFrame= () => {
+  const selectFrame = () => {
     setSelectedFrame(focus);
-  }
+  };
 
   let titulo;
-  selectedAudio ? 
-    titulo= 'Tomar una fotografía' :
-    titulo= 'Seleccionar un marco';
-  
-  if (!(!!assets?.frames)) 
+  selectedAudio
+    ? (titulo = "Tomar una fotografía")
+    : (titulo = "Seleccionar un marco");
+
+  if (!!!assets?.frames)
     return (
       <h1 className="absolute justify-center top-1/3 left-1/3">
         No frames data!
@@ -46,51 +43,50 @@ const Frames = () => {
 
   return (
     <div className="grid gap-4 h-auto my-2">
-      {!selectedImg &&
-      (<>
-        <h1 className="text-center text-2xl text-orange-500">
-          {titulo}
-        </h1>
-        <div className="flex justify-center">
-          <img 
-            className="pb-4 rounded-lg object-cover 
+      {!selectedImg && (
+        <>
+          <h1 className="text-center text-2xl text-orange-500">{titulo}</h1>
+          <div className="flex justify-center">
+            <img
+              className="pb-4 rounded-lg object-cover 
               object-center h-[50vh] fadeMe"
-            src={url}
-            alt="" />
-        </div>
+              src={url}
+              alt=""
+            />
+          </div>
 
-        {selectedAudio 
-        && <RatingStars />}
-        
-      </>)}
-      {!selectedAudio
-      && (<>
-        <section 
-          className="flex overflow-x-auto space-x-8 w-1/1">
-          { assets.frames.map( frame=> (
-            <div
-              className="flex-shrink-0 rounded-lg border-2 border-orange-300" 
-              key={frame.name}>
-              <img
-                onClick={()=> { handleUrl(frame.name) }}
-                src={`${baseURL}/img/frames/${frame.name}`}
-                className={`object-cover object-center h-20 max-w-full rounded-lg cursor-pointer fadeMe
-                ${ focus==frame.name ? 'focus: border-orange-300 border-5 border-dashed' : '' } `} 
-                alt="frame-image" 
-              />
-            </div>
-          ))}
-        </section>
+          {selectedAudio && <RatingStars />}
+        </>
+      )}
+      {!selectedAudio && (
+        <>
+          <section className="flex overflow-x-auto space-x-8 w-1/1">
+            {assets.frames.map((frame) => (
+              <div
+                className="flex-shrink-0 rounded-lg border-2 border-orange-300"
+                key={frame.name}
+              >
+                <img
+                  onClick={() => {
+                    handleUrl(frame.name);
+                  }}
+                  src={`${img_url}/${frame.name}`}
+                  className={`object-cover object-center h-20 max-w-full rounded-lg cursor-pointer fadeMe
+                ${focus == frame.name ? "focus: border-orange-300 border-5 border-dashed" : ""} `}
+                  alt="frame-image"
+                />
+              </div>
+            ))}
+          </section>
 
-        <Button
-          disabled={!focus}
-          handleOnClick={selectFrame}>
-            <FaRegCheckCircle className='mr-2' size={"1.3rem"} />
+          <Button disabled={!focus} handleOnClick={selectFrame}>
+            <FaRegCheckCircle className="mr-2" size={"1.3rem"} />
             Seleccionar
-        </Button>
-      </>)}
+          </Button>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default Frames;
--- src/services/http.js
+++ src/services/http.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 export const surveyURL = "https://mxpqsr50oid.typeform.com/to/lTr4sbqV";
-export const baseURL = "https://tipsyapi-production.up.railway.app/tipsyAPI";
-// export const baseURL= 'http://localhost:3013/tipsyAPI'
+// export const baseURL= 'https://tipsyapi-production.up.railway.app/tipsyAPI'
+export const baseURL = "http://localhost:3013/tipsyAPI";
+const appid = window.location.href.split("/")[4];
 
-export const getImageMerged = async (data) => {
-  const url = `${baseURL}/mergeImg`;
+export const getImageMerged = async (data = new FormData()) => {
+  let url = `${baseURL}/mergeImg`;
+  if (!!appid) data.append("appid", appid);
 
   const resp = await axios.post(url, data);
   if (resp.status !== 200)
@@ -15,11 +17,23 @@ export const getImageMerged = async (data) => {
 };
 
 export const getAssets = async () => {
-  const url = `${baseURL}/getAssets`;
+  console.log(appid);
+  let url;
+  !!appid
+    ? (url = `${baseURL}/getAssets/${appid}`)
+    : (url = `${baseURL}/getAssets`);
 
   const resp = await axios.get(url);
   if (resp.status !== 200)
     throw new Error("Error al obtener datos del servidor");
 
   return resp.data;
+};
+
+export const setAssetsUrl = () => {
+  let url;
+  !!appid
+    ? (url = `${baseURL}/img/event/frames`)
+    : (url = `${baseURL}/img/frames`);
+  return url;
 };
